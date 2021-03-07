a9ccd8bcf0c1d30b10a4e969fc4c32338071e6aa
Shannon Young <shannon.ari1@gmail.com>
2021-03-07 20:24:52 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -24,4 +24,18 @@ def page_count
   @page_count
 end
 
+def genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
 end
+book = Book.new("Some Title")
+book.turn_page
